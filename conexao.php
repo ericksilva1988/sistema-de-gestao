bf8d079594529ae861e3b4f5049b7f9f1e9b888d
Denys Rocha <contato.denysrocha@gmail.com>
2023-01-27 08:17:37 -0300

    criando funções para listar todos e pesquisar

--- conexao.php
+++ conexao.php
@@ -1,25 +1,48 @@
 <?php
 
-	class db {
+class db
+{
 
-		private $hostname = 'localhost';
+	private $hostname = 'localhost';
 
-		private $usuario = 'root';
+	private $usuario = 'root';
 
-		private $senha = '';
+	private $senha = '';
 
-		private $database = 'sistema-comercial';
+	private $database = 'sistema-comercial';
 
 
 
-		public function conecta_mysql(){
+	public function conecta_mysql()
+	{
 
-			$conexao = mysqli_connect($this->hostname, $this->usuario, $this->senha, $this->database);
+		$conexao = mysqli_connect($this->hostname, $this->usuario, $this->senha, $this->database);
 
-			return $conexao;
-		}
-		
+		return $conexao;
 	}
+
+}
+
+class retornaDados
+{
+	public function retornaTodos($tabela)
+	{
+		$database = new db();
+		$conexao = $database->conecta_mysql();
+		$query = "select * from `$tabela` order by id desc";
+		$result = mysqli_query($conexao, $query);
+		return $result;
+	}
+
+	public function pesquisar($tabela, $termoDeBusca, $coluna1, $coluna2, $coluna3)
+	{
+		$database = new db();
+		$conexao = $database->conecta_mysql();
+		$query = "select * FROM `$tabela` WHERE `$coluna1` LIKE `%$termoDeBusca%` or `$coluna2` LIKE `%$termoDeBusca%` or `$coluna3` LIKE `%$termoDeBusca%` ORDER BY `$coluna1` DESC";
+		$result = mysqli_query($conexao, $query);
+		return $result;
+	}
+}
 
 
 ?>
--- usuario/listar.php
+++ usuario/listar.php
@@ -1,50 +1,38 @@
 <?php
-    
-    session_start();
 
-    if ((!isset($_SESSION['login']) == true) and (!isset($_SESSION['senha']) == true)) {
-        
-        unset($_SESSION['login']);
-        unset($_SESSION['senha']);
-                                
-        header('Location: listar.php');
+session_start();
 
-        $login = $_SESSION['login'];
+if ((!isset($_SESSION['login']) == true) and (!isset($_SESSION['senha']) == true)) {
+
+    unset($_SESSION['login']);
+    unset($_SESSION['senha']);
+
+    header('Location: listar.php');
+
+    $login = $_SESSION['login'];
 
 
-    }
+}
 
-    
-    include_once("../conexao.php");
 
-    //$query = "select * from usuarios order by id desc";
-
-                $database = new db();
-
-                $conexao = $database-> conecta_mysql();
-
-                $query = "select * from usuarios order by id desc";
-
-                $result = mysqli_query ($conexao, $query);
-    
-                //$nome = $row['nome'];
-                
+include_once("../conexao.php");
+$database = new retornaDados();
+$result = $database->retornaTodos('usuarios');
 
 
 
-        if (!empty($_GET['pesquisar'])) {
+if (!empty($_GET['pesquisar'])) {
 
-            $termoDeBusca = $_GET['pesquisar'];
-            $queryBusca = "SELECT * FROM usuarios WHERE id LIKE '%$termoDeBusca%' or login LIKE '%$termoDeBusca%' or senha LIKE '%$termoDeBusca%' ORDER BY id DESC";         
-            $result = mysqli_query ($conexao, $queryBusca);
-            
-            $msg = "Registros Encontrados";
-        }else {
-            
-            $msg = "Sem buscas aplicadas!";
-        }
-      
-    
+    $termoDeBusca = $_GET['pesquisar'];
+    $result = $database->pesquisar('usuarios', $termoDeBusca, 'id', 'nome', 'login');
+
+    $msg = "Registros Encontrados";
+} else {
+
+    $msg = "Sem buscas aplicadas!";
+}
+
+
 ?>
 
 
@@ -81,43 +69,51 @@
                         <a class="nav-link active" aria-current="page" href="usuario.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a data-bs-toggle='modal' data-bs-target='#modalSair'class="nav-link" href="../sair.php">Sair</a>
+                        <a data-bs-toggle='modal' data-bs-target='#modalSair' class="nav-link"
+                            href="../sair.php">Sair</a>
                     </li>
-                    
+
                 </ul>
-                <p><?php  echo "Bem Vindo: *" ?></p> <p class=><b><?php echo $_SESSION['login']; ?></b></p>
+                <p>
+                    <?php echo "Bem Vindo: *" ?>
+                </p>
+                <p class=><b>
+                        <?php echo $_SESSION['login']; ?>
+                    </b></p>
             </div>
         </div>
     </nav>
 
     <!-- Modal -->
-       
-        <div class="modal fade" id="modalSair" tabindex="-1" aria-labelledby="modalSair" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalSair">Atenção!</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                       <b><?php echo $_SESSION['login']; ?></b> Tem certeza que deseja sair da sessão?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancelar</button>
-                        <a href="../sair.php">
+
+    <div class="modal fade" id="modalSair" tabindex="-1" aria-labelledby="modalSair" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalSair">Atenção!</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <b>
+                        <?php echo $_SESSION['login']; ?>
+                    </b> Tem certeza que deseja sair da sessão?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancelar</button>
+                    <a href="../sair.php">
                         <button type="button" class="btn btn-danger">Sair</button>
-                        </a>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <!-- /Modal -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
-    
+
     <!-- /Menu superior-->
 
 
@@ -128,16 +124,24 @@
                 <a href="cadastrar.php" class="btn btn-primary">Cadastrar</a>
             </div>
             <div class="col-md-5 col-sm-12">
-                <input class="form-control" type="search" placeholder="Buscar usuário" id="pesquisar" name="pesquisar" aria-label="Search">
+                <input class="form-control" type="search" placeholder="Buscar usuário" id="pesquisar" name="pesquisar"
+                    aria-label="Search">
             </div>
-            
+
             <div class="col-md-3 col-lg-2 mb-2">
-                <button onclick="searchData()"  class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+                <button onclick="searchData()" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
+                        fill="#000000">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path
+                            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                    </svg>
                 </button>
             </div>
         </div>
-        <p><?php echo $msg; ?></p>
+        <p>
+            <?php echo $msg; ?>
+        </p>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -150,18 +154,18 @@
                 </tr>
             </thead>
             <tbody>
-                
-                <?php 
 
-                        while($user_data = mysqli_fetch_assoc($result)) {
-                            
-                            echo "<tr>";
-                            echo "<td>" . $user_data['id'] . "</td>";
-                            echo "<td>" . $user_data['nome'] . "</td>";
-                            echo "<td>" . $user_data['login'] . "</td>";
-                            echo "<td>" . $user_data['senha'] . "</td>";
-                            
-                            echo "<td>
+                <?php
+
+                while ($user_data = mysqli_fetch_assoc($result)) {
+
+                    echo "<tr>";
+                    echo "<td>" . $user_data['id'] . "</td>";
+                    echo "<td>" . $user_data['nome'] . "</td>";
+                    echo "<td>" . $user_data['login'] . "</td>";
+                    echo "<td>" . $user_data['senha'] . "</td>";
+
+                    echo "<td>
                                 <a href='formulario.php?id=$user_data[id]'>
                                   <button class='btn btn-light'>Editar</button>
                                 </a>
@@ -170,8 +174,8 @@
                                 </a>
 
                             </td>";
-                            echo "</tr>";
-                        }
+                    echo "</tr>";
+                }
 
                 ?>
             </tbody>
@@ -197,42 +201,42 @@
             </div>
         </div>
     </div>
-<?php  
+    <?php
 
-?>
+    ?>
     <!-- /Modal -->
 
-        <script type="text/javascript">
-    
-            function passaDadosModal(id, nome){
-               
-                document.querySelector('#nome-usuario').innerText=nome;
-                document.querySelector('#linkExcluir').href="excluir.php?id="+id;
-                                
+    <script type="text/javascript">
+
+        function passaDadosModal(id, nome) {
+
+            document.querySelector('#nome-usuario').innerText = nome;
+            document.querySelector('#linkExcluir').href = "excluir.php?id=" + id;
+
+        }
+
+
+    </script>
+
+
+
+    <script type="text/javascript">
+
+        var pesquisar = document.getElementById('pesquisar');
+
+        pesquisar.addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                fazBusca();
             }
-
-            
-        </script>
+        });
 
 
-
-        <script type="text/javascript">
-            
-            var pesquisar = document.getElementById('pesquisar');
-            
-            pesquisar.addEventListener("keydown", function(event) {
-                if (event.key ==="Enter"){
-                    fazBusca();
-                }
-            });
+        function fazBusca() {
+            window.location = 'listar.php?pesquisar=' + pesquisar.value;
+        }
 
 
-            function fazBusca() {
-            window.location = 'listar.php?pesquisar='+pesquisar.value;
-            }
-
- 
-        </script>
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
